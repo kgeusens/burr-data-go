d4a4e6d9450c231af9acd3e6bf60923b0d108f92
Koen Geusens <koen_geusens@hotmail.com>
2023-11-10 09:59:40 +0100

    problem to separate file

--- xmpuzzle/xmpuzzle.go
+++ xmpuzzle/xmpuzzle.go
@@ -76,36 +76,6 @@ func (s *State) Z() []string {
 	return strings.Split(s.DZ.Text, " ")
 }
 
-type Problem struct {
-	XMLName       xml.Name   `xml:"problem"`
-	Name          string     `xml:"name,attr"`
-	State         int        `xml:"state,attr"`
-	Assemblies    int        `xml:"assemblies,attr"`
-	SolutionCount int        `xml:"solutions,attr"`
-	Time          int        `xml:"time,attr"`
-	Shapes        []Shape    `xml:"shapes>shape"`
-	Result        Result     `xml:"result"`
-	Solutions     []Solution `xml:"solutions>solution"`
-	Comment       Comment    `xml:"comment"`
-}
-
-func (p *Problem) NumShapes() int {
-	return len(p.Shapes)
-}
-
-func (p *Problem) GetShapemap() (result []int) {
-	for _, shape := range p.Shapes {
-		count := shape.Count
-		if count == 0 {
-			count = shape.Max
-		}
-		for i := 0; i < count; i++ {
-			result = append(result, shape.Id)
-		}
-	}
-	return
-}
-
 type Comment struct {
 	XMLName xml.Name `xml:"comment"`
 	Text    string   `xml:",chardata"`
