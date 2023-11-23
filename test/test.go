5220437c38775e7f82eefb9a44400faccd95136d
Koen Geusens <koen_geusens@hotmail.com>
2023-11-23 15:32:11 +0100

    stop profiling

--- test/test.go
+++ test/test.go
@@ -12,8 +12,8 @@ import (
 	"github.com/kgeusens/go/burr-data/xmpuzzle"
 )
 
-var cpuprofile = flag.String("cpuprofile", "cpu.prof", "write cpu profile to `file`")
-var memprofile = flag.String("memprofile", "mem.prof", "write memory profile to `file`")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
 
